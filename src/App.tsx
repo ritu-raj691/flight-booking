c67543bea487dbd1e5e8f188bd490e05967e36c9
ritu-raj691 <rituraj691@gmail.com>
2024-03-21 15:55:19 +0530

    Moved filterBar App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Button } from "@mui/material";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { Button, Typography } from "@mui/material";
+import { Dayjs } from "dayjs";
+import { options } from "./Constant/Constant";
+import { ITravelers } from "./Model/Travelers";
 import Home from "./Components/Home/Home";
 import Navbar from "./Common/Navbar/Navbar";
 import "./App.css";
 const SearchList = lazy(() => import("./Components/SearchDeal/SearchList"));
+const FilterBar = lazy(() => import("./Components/FilterBar/FilterBar"));
 const TravelerDetail = lazy(
   () => import("./Components/TravelerDetails/TravelerDetail")
 );
@@ -15,10 +24,50 @@ const Login = lazy(() => import("./Components/Login/Login"));
 const App = () => {
   const [isSignUpModalOpen, setIsSignUpModalOpen] = useState<boolean>(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
+  const [isFilterBarDisabled, setIsFilterBarDisabled] =
+    useState<boolean>(false);
+  const [selectedDate, setSelectedDate] = useState<Date | string>(
+    new Date()?.toLocaleDateString("en-US", options as any)
+  );
+  const [allTravelers, setAllTravelers] = useState<ITravelers>({
+    adults: 1,
+    children: 0,
+    infants: 0,
+  });
+
+  const TrackRouteChanges = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+      // Perform actions when route changes
+      if (
+        location?.pathname?.includes("/package") ||
+        location?.pathname?.includes("/traveler-detail")
+      ) {
+        setIsFilterBarDisabled(true);
+      } else {
+        setIsFilterBarDisabled(false);
+      }
+    }, [location.pathname]);
+
+    return null;
+  };
+
+  const currentSelectedDate = (currentSelectedDate: Dayjs | null) => {
+    const currentDate = new Date(
+      currentSelectedDate?.toString() ?? ""
+    )?.toLocaleDateString("en-US", options as any);
+    setSelectedDate(currentDate);
+  };
+
+  const selectedPassengers = (travelers: ITravelers) => {
+    setAllTravelers(travelers);
+  };
 
   return (
     <>
       <Router>
+        <TrackRouteChanges />
         <Button onClick={() => setIsLoginModalOpen(true)}>Login</Button>
         <Button onClick={() => setIsSignUpModalOpen(true)}>SignUp</Button>
         <Suspense fallback={<></>}>
@@ -35,6 +84,19 @@ const App = () => {
         </Suspense>
         <Navbar />
         <div className="container">
+          {!isFilterBarDisabled && (
+            <>
+              <Typography variant="h6" className="air-fares-label">
+                Search for Series AIR Fares
+              </Typography>
+              <Suspense fallback={<></>}>
+                <FilterBar
+                  newSelectedDate={currentSelectedDate}
+                  selectedPassengers={selectedPassengers}
+                />
+              </Suspense>
+            </>
+          )}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/flights" element={<Home />} />
@@ -42,7 +104,10 @@ const App = () => {
               path="/search-flight"
               element={
                 <Suspense fallback={<></>}>
-                  <SearchList />{" "}
+                  <SearchList
+                    allTravelers={allTravelers}
+                    selectedDate={selectedDate}
+                  />{" "}
                 </Suspense>
               }
             />
@@ -65,7 +130,6 @@ const App = () => {
               </Suspense>
             }
           />
-          <Route path="*" element={<>Page Not Found</>} />
         </Routes>
       </Router>
     </>
--- src/Components/SearchDeal/SearchList.tsx
+++ src/Components/SearchDeal/SearchList.tsx
@@ -1,39 +1,19 @@
-import React, { useState, lazy, Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { Typography } from "@mui/material";
-import { Dayjs } from "dayjs";
-import { options } from "../../Constant/Constant";
+
 import { ITravelers } from "../../Model/Travelers";
-const FilterBar = lazy(() => import("../FilterBar/FilterBar"));
 const FlightDetailCard = lazy(() => import("../SearchDeal/FlightDetailCard"));
 
-const SearchList = () => {
-  const [selectedDate, setSelectedDate] = useState<Date | string>(
-    new Date()?.toLocaleDateString("en-US", options as any)
-  );
-  const [allTravelers, setAllTravelers] = useState<ITravelers>();
+interface ISearchList {
+  allTravelers: ITravelers;
+  selectedDate: Date | string;
+}
 
-  const currentSelectedDate = (currentSelectedDate: Dayjs | null) => {
-    const currentDate = new Date(
-      currentSelectedDate?.toString() ?? ""
-    )?.toLocaleDateString("en-US", options as any);
-    setSelectedDate(currentDate);
-  };
-
-  const selectedPassengers = (travelers: ITravelers) => {
-    setAllTravelers(travelers);
-  };
+const SearchList = (props: ISearchList) => {
+  const { allTravelers, selectedDate } = props;
 
   return (
     <>
-      <Typography variant="h6" className="air-fares-label">
-        Search for Series AIR Fares
-      </Typography>
-      <Suspense fallback={<></>}>
-        <FilterBar
-          newSelectedDate={currentSelectedDate}
-          selectedPassengers={selectedPassengers}
-        />
-      </Suspense>
       <div className="selected-date-section">
         <Typography variant="subtitle2" className="selected-date">
           {selectedDate as any}
